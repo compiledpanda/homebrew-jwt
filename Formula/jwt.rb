1f5fc00541c6adfa1541efff30389ad25bf9880d
John Clark <socrates37@gmail.com>
2019-09-16 06:31:57 -0600

    Release v0.1.2

--- Formula/jwt.rb
+++ Formula/jwt.rb
@@ -1,10 +1,10 @@
 class Jwt < Formula
   desc "CLI to encode, decode, and validate JWTs"
   homepage "https://github.com/compiledpanda/jwt"
-  version "v0.1.1"
+  version "v0.1.2"
 
-  url "https://github.com/compiledpanda/jwt/releases/download/v0.1.1/jwt_0.1.1_Darwin_x86_64.tar.gz"
-  sha256 "a74363a3c13755f6fbc0b6bb5dc4c8b7430aef5856bbd2102b8eff91fec3e702"
+  url "https://github.com/compiledpanda/jwt/releases/download/v0.1.2/jwt_0.1.2_Darwin_x86_64.tar.gz"
+  sha256 "d615c70c91461b082e2b2b174517792dab8fba8428839b2880f1770568ba6efe"
 
   def install
     bin.install "jwt"
