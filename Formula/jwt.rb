e964a2c2c7aac6421820795ebfb80c6a9a3e66ff
John Clark <socrates37@gmail.com>
2019-09-16 06:01:11 -0600

    add homebrew tap

---
+++ Formula/jwt.rb
@@ -0,0 +1,12 @@
+class Jwt < Formula
+  desc "CLI to encode, decode, and validate JWTs"
+  homepage "https://github.com/compiledpanda/jwt"
+  version "v0.1.1"
+
+  url "https://github.com/compiledpanda/jwt/releases/download/v0.1.1/jwt_0.1.1_Darwin_x86_64.tar.gz"
+  sha256 "a74363a3c13755f6fbc0b6bb5dc4c8b7430aef5856bbd2102b8eff91fec3e702"
+
+  def install
+    bin.install "program"
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # homebrew-jwt
 Homebrew tap for compiledpanda/jwt
+
+```
+$> brew tap compiledpanda/jwt
+$> brew install jwt
+$> jwt
+```
